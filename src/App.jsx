edd05752949363b5f34a089abddae201b8165d86
Bharath Raj <gamebharath520@gmail.com>
2026-05-25 10:18:08 +0530

    Initial website commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <div>
+        <h1 className="text-5xl font-bold">
+          Welcome Bharath 🚀
+        </h1>
+
+        <p className="mt-4 text-gray-400">
+          My first React website
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
